7ebef81abb0b98032ae4a15e040af968b6b0416d
Pathum Athukorala <pathum.athukorala@ebuilder.com>
2016-05-25 08:08:02 +0530

    add trim function

--- src/com/ebuilder/git/training2/MainRunner.java
+++ src/com/ebuilder/git/training2/MainRunner.java
@@ -7,5 +7,9 @@ public class MainRunner {
 		System.out.print("This is the best day of my life!");
 		//System.out.println("this is not committed yet");
 	}
+	
+	private String trimString(String untrimmed) {
+		return untrimmed.trim();
+	}
 
 }
